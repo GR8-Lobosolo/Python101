0c3dffcc234428ebb4c7ee80cdf1fa66dfb45c28
gibson.business@gmail.com <gibson.business@gmail.com>
2018-07-12 22:18:10 -0400

    random unsaved work

---
+++ playground.py
@@ -0,0 +1,22 @@
+print("Hello Galaxy")
+
+
+words = ["spam", "egg", "spam", "sausage"]
+print("spam" in words)
+print("egg" in words)
+print("tomato" in words)
+
+
+
+letters = ['p', 'q', 'r', 's', 'p', 'u']
+print(letters.index('r'))
+print(letters.index('p'))
+print(letters.index('z'))
+
+max(letters)
+print(letters.count('p'))
+print(letters)
+
+
+numbers = list(range(10))
+print(numbers)
